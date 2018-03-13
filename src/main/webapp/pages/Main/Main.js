5e659b5f1be80f061e22c36725a201ca103cbf9b
Sergey Shablenko <shablenko@sednor.net>
2018-03-13 15:46:59 +0000

    in progress New files added - [salesdb,FacebookOAuth,data,resources] Files modified - [Main,profiles,pom.xml,project-services.xml,config.json]

--- src/main/webapp/pages/Main/Main.js
+++ src/main/webapp/pages/Main/Main.js
@@ -1,28 +1,82 @@
-Application.$controller("kendo_data_tableController", ["$scope", function ($scope) {
-	"use strict";
+Application.$controller("Kendo_data_tableController", ["$scope", function($scope) {
+    "use strict";
 
-	/*
-	 * This function will be invoked when any of this prefab's property is changed
-	 * @key: property name
-	 * @newVal: new value of the property
-	 * @oldVal: old value of the property
-	 */
-	function propertyChangeHandler(key, newVal, oldVal) {
-		/*
-		switch (key) {
-			case "prop1":
-				// do something with newVal for property 'prop1'
-				break;
-			case "prop2":
-				// do something with newVal for property 'prop2'
-				break;
-		}
-		*/
-	}
-	/* register the property change handler */
-	$scope.propertyManager.add($scope.propertyManager.ACTIONS.CHANGE, propertyChangeHandler);
+    /*
+     * This function will be invoked when any of this prefab's property is changed
+     * @key: property name
+     * @newVal: new value of the property
+     * @oldVal: old value of the property
+     */
+    function propertyChangeHandler(key, newVal, oldVal) {
 
-	$scope.onInitPrefab = function() {
-		// this method will be triggered post initialization of the prefab.
-	};
+    }
+    /* register the property change handler */
+    $scope.propertyManager.add($scope.propertyManager.ACTIONS.CHANGE, propertyChangeHandler);
+
+    $scope.onInitPrefab = function() {};
+
+    $scope.onPageReady = function() {
+        let once = false;
+
+        $(`#${$scope.Variables.kendoTableId.getData().dataValue}`).kendoGrid({
+            dataSource: {
+                transport: {
+                    read: {
+                        url: $scope.Variables.fetchUrl.getData().dataValue,
+                    },
+                    create: {
+                        url: $scope.Variables.updateUrl.getData().dataValue,
+                        type: "post"
+                    },
+                    update: {
+                        url: $scope.Variables.updateUrl.getData().dataValue,
+                        type: "update"
+                    },
+                    destroy: {
+                        url: $scope.Variables.updateUrl.getData().dataValue,
+                        type: "delete"
+                    },
+                    parameterMap: function(data, type) {
+                        console.log(data);
+                        if (data.hasOwnProperty("pageSize")) {
+                            const DATA = Object.assign({}, data, {
+                                size: data.pageSize
+                            });
+
+                            DATA.pageSize = undefined;
+                            return DATA;
+                        }
+                    }
+                },
+                schema: {
+                    data: "content"
+                },
+                serverPaging: true,
+                pageSize: 2,
+            },
+            dataBound: function() {
+                if (once) {
+                    return;
+                }
+                const OPTIONS = this.getOptions();
+
+                this.setOptions(Object.assign({}, OPTIONS, {
+                    columns: [].concat(OPTIONS.columns, {
+                        command: ["edit", "destroy"]
+                    })
+                }));
+                once = true;
+            },
+            saveChanges: function() {
+                console.log('asdasdsdas');
+            },
+            toolbar: ["create"],
+            pageable: {
+                refresh: true,
+                pageSizes: true,
+                buttonCount: 5
+            },
+            editable: "popup"
+        });
+    }
 }]);
